d60191fd52ea5c06f6d4b2165e49adf2a8beea8b
ryanprince-code <ryanprince8019@gmail.com>
2024-11-06 11:36:46 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pc7)ae@0enj6ii1cpi5mb_g&a)i8s+&l8j-_fiqwvb^3$e9224'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','ryanc0de.pythonanywhere.com']
 
